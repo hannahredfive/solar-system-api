dd0889d8ca59b71567f7d7fea9f5d81b2b7e8b9e
Hannah <hannah@chandleys.com>
2023-04-20 12:11:09 -0700

    Created Planet class, created a list of planets in our Solar System, created a route/endpoint for all planets and registered this endpoint in our __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, Blueprint
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
+
+    from .routes import planets_bp
+    app.register_blueprint(planets_bp)
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,36 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+class Planet():
+    def __init__(self, id, name, description, moon_count):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.moon_count = moon_count
+
+planets = [
+    Planet(1, "Mercury", "Closest planet to the sun, has no atmosphere, and revolves around the sun faster than any other planet in our Solar System.", 0),
+    Planet(2, "Venus", "Second closest planet to the sun, is the only planet in our Solar System besides Earth that has a substantial atmosphere.", 0),
+    Planet(3, "Earth", "Third from the Sun and the only known place in the universe where life has originated and found habitability.", 1),
+    Planet(4, "Mars", "Fourth planet from the Sun and the third largest and massive terrestrial object in the Solar System.", 2),
+    Planet(5, "Jupiter", " Jupiter is more than twice as massive than the other planets of our solar system combined. The giant planet's Great Red Spot is a centuries-old storm bigger than Earth.", 95),
+    Planet(6, "Saturn", "Adorned with a dazzling, complex system of icy rings, Saturn is unique in our solar system. The other giant planets have rings, but none are as spectacular as Saturn's.", 83),
+    Planet(7, "Uranus", "Uranus—seventh planet from the Sun—rotates at a nearly 90-degree angle from the plane of its orbit. This unique tilt makes Uranus appear to spin on its side.", 27),
+    Planet(8, "Neptune", "Neptune—the eighth and most distant major planet orbiting our Sun—is dark, cold and whipped by supersonic winds. It was the first planet located through mathematical calculations.", 14)
+]
+
+
+'''ENDPOINTS/ROUTES BELOW'''
+
+@planets_bp.route("", methods=["GET"])
+def handle_planets():
+    planets_response = []
+    for planet in planets:
+        planets_response.append({
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "number of moons": planet.moon_count
+        })
+    return jsonify(planets_response)
