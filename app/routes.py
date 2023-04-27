6b2ff9d47807a62003470bd49efb78e9cc84abc0
Hannah <hannah@chandleys.com>
2023-04-27 14:47:43 -0700

    Created database for Solar System, implimented post and get for 'planets', post and get are fully functional and are implimented separately.

--- app/routes.py
+++ app/routes.py
@@ -1,64 +1,38 @@
-from flask import Blueprint, jsonify, abort, make_response
+from flask import Blueprint, jsonify, abort, make_response, request
+from app.models.planet import Planet
+from app import db
+
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
-class Planet():
-    def __init__(self, id, name, description, moon_count):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.moon_count = moon_count
-
-planets = [
-    Planet(1, "Mercury", "Closest planet to the sun, has no atmosphere, and revolves around the sun faster than any other planet in our Solar System.", 0),
-    Planet(2, "Venus", "Second closest planet to the sun, is the only planet in our Solar System besides Earth that has a substantial atmosphere.", 0),
-    Planet(3, "Earth", "Third from the Sun and the only known place in the universe where life has originated and found habitability.", 1),
-    Planet(4, "Mars", "Fourth planet from the Sun and the third largest and massive terrestrial object in the Solar System.", 2),
-    Planet(5, "Jupiter", " Jupiter is more than twice as massive than the other planets of our solar system combined. The giant planet's Great Red Spot is a centuries-old storm bigger than Earth.", 95),
-    Planet(6, "Saturn", "Adorned with a dazzling, complex system of icy rings, Saturn is unique in our solar system. The other giant planets have rings, but none are as spectacular as Saturn's.", 83),
-    Planet(7, "Uranus", "Uranus—seventh planet from the Sun—rotates at a nearly 90-degree angle from the plane of its orbit. This unique tilt makes Uranus appear to spin on its side.", 27),
-    Planet(8, "Neptune", "Neptune—the eighth and most distant major planet orbiting our Sun—is dark, cold and whipped by supersonic winds. It was the first planet located through mathematical calculations.", 14)
-]
-
-# helper function validates planet: (1. checks if planet id type is invalid, 2. checks if)
-# planet is non-existing, 3. checks if planet exists, return planet
-def validate_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except:
-        abort(make_response({"msg":f"planet {planet_id} invalid"}, 400))
-    
-    for planet in planets:
-        if planet.id == planet_id:
-            return planet
-    
-    abort(make_response({"msg": f"planet {planet_id} not found"}, 404))
-
 '''ENDPOINTS/ROUTES BELOW'''
 
-# wave 1
+@planets_bp.route("", methods=["POST"])
+def create_planet():
+    request_body = request.get_json()
+    new_planet = Planet(
+        name = request_body["name"],
+        description = request_body["description"],
+        moon_count = request_body["moon_count"]
+    )
+
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f"Planet {new_planet.name} successfully created.", 201)
+
 @planets_bp.route("", methods=["GET"])
-def handle_planets():
+def get_all_planets():
     planets_response = []
+    planets = Planet.query.all()
     for planet in planets:
-        planets_response.append({
-            "id": planet.id,
-            "name": planet.name,
-            "description": planet.description,
-            "number of moons": planet.moon_count
-        })
+        planets_response.append(
+            {
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description,
+                "moon_count": planet.moon_count
+            }
+        )
+    
     return jsonify(planets_response)
-
-
-# wave 2 refactored hand_planet func using helper func
-@planets_bp.route("/<planet_id>", methods=["GET"])
-def handle_planet(planet_id):
-    planet = validate_planet(planet_id)
-
-    return jsonify({
-        "id": planet.id,
-        "name": planet.name,
-        "description": planet.description,
-        "number of moons": planet.moon_count
-    })
-
