2172e84cbaf559509c63a339f01233cbd0d78720
Ariel Williams <arielwilliams121013@gmail.com>
2023-05-01 13:45:35 -0700

    Implemented GET, PUT, and DELETE routes for specific planets and made a helper function called validate_planet() to handle 400 and 404 errors.

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,21 @@ from app import db
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
+# helper functions
+def validate_planet(planet_id):
+    try: 
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+
+    planet = Planet.query.get(planet_id)
+
+    if not planet:
+        abort(make_response({"message":f"book {planet_id} not found"}, 404))
+
+    return planet
+
+
 '''ENDPOINTS/ROUTES BELOW'''
 
 @planets_bp.route("", methods=["POST"])
@@ -36,3 +51,39 @@ def get_all_planets():
         )
     
     return jsonify(planets_response)
+
+
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def read_one_planet(planet_id):
+    planet = validate_planet(planet_id)
+    return {
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "moon_count": planet.moon_count
+        }, 200
+
+
+@planets_bp.route("/<planet_id>", methods=["PUT"])
+def update_one_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    request_body = request.get_json()
+
+    planet.name = request_body["name"]
+    planet.description = request_body["description"]
+    planet.moon_count = request_body["moon_count"]
+
+    db.session.commit()
+
+    return make_response(f"Planet #{planet.id} successfully updated", 200)
+
+
+@planets_bp.route("/<planet_id>", methods=["DELETE"])
+def delete_one_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    db.session.delete(planet)
+    db.session.commit()
+
+    return make_response(f"Planet #{planet.id} successfully deleted", 200)
