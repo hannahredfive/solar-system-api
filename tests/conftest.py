6073b43c6d13b3f67660d0b962b613ee2a188f06
Hannah <hannah@chandleys.com>
2023-05-05 14:50:53 -0700

    Refactoring: Created more tests for get all planets and made two_saved_planets more readable in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,12 +28,20 @@ def client(app):
 @pytest.fixture
 def two_saved_planets(app):
     # Arrange
-    mercury_planet = Planet(name="Mercury", description="First planet from the sun", moon_count=0)
-    earth_planet = Planet(name="Earth", description="Third planet from the sun", moon_count=1)
+    mercury_planet = Planet(
+        name="Mercury", 
+        description="First planet from the sun", 
+        moon_count=0
+    )
+    earth_planet = Planet(
+        name="Earth", 
+        description="Third planet from the sun", 
+        moon_count=1
+    )
 
-    db.session.add_all([mercury_planet, earth_planet])
+    # db.session.add_all([mercury_planet, earth_planet])
     # Alternatively, we could do
-    # db.session.add(ocean_book)
-    # db.session.add(mountain_book)
+    db.session.add(mercury_planet)
+    db.session.add(earth_planet)
     db.session.commit()
 
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -9,6 +9,55 @@ def test_get_all_planets_with_no_records(client):
     assert response.status_code == 200
     assert response_body == [] 
 
+def test_get_all_planets_with_records(client, two_saved_planets):
+    # Act
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == [
+        {
+            "id": 1,
+            "name": "Mercury",
+            "description": "First planet from the sun",
+            "moon_count": 0
+        },
+        {
+            "id": 2,
+            "name": "Earth",
+            "description": "Third planet from the sun",
+            "moon_count": 1
+        }
+    ]
+
+def test_get_all_planets_with_name_query_no_match(client, two_saved_planets):
+    # Act
+    data = {"name": "Pluto"}
+    response = client.get("/planets", query_string = data)
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
+
+def test_get_all_planets_with_records_and_query_param_matching_returns_planet(client, two_saved_planets):
+    # Act
+    data = {"name": "Mercury"}
+    response = client.get("/planets", query_string = data)
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == [
+        {
+            "id": 1,
+            "name": "Mercury",
+            "description": "First planet from the sun",
+            "moon_count": 0
+        }
+    ]
+
 # get one planet by id
 def test_get_one_planet(client, two_saved_planets):
     # Act
