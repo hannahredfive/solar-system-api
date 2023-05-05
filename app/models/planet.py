35e12c2b6cdd374af85813602d4a29d2532cbed8
Hannah <hannah@chandleys.com>
2023-05-05 15:08:49 -0700

    Created tests for to_dict and implented method in class

--- app/models/planet.py
+++ app/models/planet.py
@@ -5,3 +5,11 @@ class Planet(db.Model):
     name = db.Column(db.String)
     description = db.Column(db.String)
     moon_count = db.Column(db.Integer)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "moon_count": self.moon_count
+        }
---
+++ tests/test_models.py
@@ -0,0 +1,70 @@
+from app.models.planet import Planet
+
+def test_to_dict_no_missing_data():
+    # Arrange
+    test_data = Planet(
+        id = 1,
+        name="Mercury",
+        description="First planet from the sun",
+        moon_count=0
+    )
+
+    # Act
+    result = test_data.to_dict()
+
+    # Assert
+    assert len(result) == 4
+    assert result["id"] == 1
+    assert result["name"] == "Mercury"
+    assert result["description"] == "First planet from the sun"
+    assert result["moon_count"] == 0
+
+def test_to_dict_missing_id():
+    # Arrange
+    test_data = Planet(
+        name="Mercury", 
+        description="First planet from the sun",
+        moon_count=0
+    )
+
+    # Act
+    result = test_data.to_dict()
+
+    # Assert
+    assert len(result) == 4
+    assert result["id"] is None
+    assert result["name"] == "Mercury"
+    assert result["description"] == "First planet from the sun"
+    assert result["moon_count"] == 0
+
+def test_to_dict_missing_name():
+    # Arrange
+    test_data = Planet(id=1, description="First planet", moon_count=0)
+
+    # Act
+    result = test_data.to_dict()
+
+    # Assert
+    assert len(result) == 4
+    assert result["id"] == 1
+    assert result["name"] is None
+    assert result["description"] == "First planet"
+    assert result["moon_count"] == 0
+
+def test_to_dict_missing_description():
+    # Arrange
+    test_data = Planet(
+        id = 1,
+        name="Mercury",
+        moon_count=0
+    )
+
+    # Act
+    result = test_data.to_dict()
+
+    # Assert
+    assert len(result) == 4
+    assert result["id"] == 1
+    assert result["name"] == "Mercury"
+    assert result["description"] is None
+    assert result["moon_count"] == 0
